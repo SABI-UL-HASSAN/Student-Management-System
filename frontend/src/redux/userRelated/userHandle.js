92c5d725d45d53cf706369590dd13bde97abad24
Sabi Ul Hassan <sabihassan911@gmail.com>
2025-02-10 06:51:59 +0500

    first commit

---
+++ frontend/src/redux/userRelated/userHandle.js
@@ -0,0 +1,113 @@
+import axios from 'axios';
+import {
+    authRequest,
+    stuffAdded,
+    authSuccess,
+    authFailed,
+    authError,
+    authLogout,
+    doneSuccess,
+    getRequest,
+    getFailed,
+    getError,
+} from './userSlice';
+
+// Login User
+export const loginUser = (fields, role) => async (dispatch) => {
+    dispatch(authRequest());
+
+    try {
+        const result = await axios.post(`${process.env.REACT_APP_BASE_URL}/${role}Login`, fields, {
+            headers: { 'Content-Type': 'application/json' },
+        });
+        if (result.data.role) {
+            dispatch(authSuccess(result.data));
+        } else {
+            dispatch(authFailed(result.data.message));
+        }
+    } catch (error) {
+        dispatch(authError(error));
+    }
+};
+
+// Register User
+export const registerUser = (fields, role) => async (dispatch) => {
+    dispatch(authRequest());
+
+    try {
+        const result = await axios.post(`${process.env.REACT_APP_BASE_URL}/${role}Reg`, fields, {
+            headers: { 'Content-Type': 'application/json' },
+        });
+        if (result.data.schoolName) {
+            dispatch(authSuccess(result.data));
+        } else if (result.data.school) {
+            dispatch(stuffAdded());
+        } else {
+            dispatch(authFailed(result.data.message));
+        }
+    } catch (error) {
+        dispatch(authError(error));
+    }
+};
+
+// Logout User
+export const logoutUser = () => (dispatch) => {
+    dispatch(authLogout());
+};
+
+// Get User Details
+export const getUserDetails = (id, address) => async (dispatch) => {
+    dispatch(getRequest());
+
+    try {
+        const result = await axios.get(`${process.env.REACT_APP_BASE_URL}/${address}/${id}`);
+        if (result.data) {
+            dispatch(doneSuccess(result.data));
+        }
+    } catch (error) {
+        dispatch(getError(error));
+    }
+};
+
+// Delete User (Currently Disabled)
+export const deleteUser = (id, address) => async (dispatch) => {
+    dispatch(getRequest());
+    dispatch(getFailed("Sorry, the delete function has been disabled for now."));
+};
+
+// Update User
+export const updateUser = (fields, id, address) => async (dispatch) => {
+    dispatch(getRequest());
+
+    try {
+        const result = await axios.put(`${process.env.REACT_APP_BASE_URL}/${address}/${id}`, fields, {
+            headers: { 'Content-Type': 'application/json' },
+        });
+        if (result.data.schoolName) {
+            dispatch(authSuccess(result.data));
+        } else {
+            dispatch(doneSuccess(result.data));
+        }
+    } catch (error) {
+        dispatch(getError(error));
+    }
+};
+
+// Add Stuff (e.g., New User or Resource)
+export const addStuff = (fields, address) => async (dispatch) => {
+    dispatch(authRequest());
+
+    try {
+        const result = await axios.post(`${process.env.REACT_APP_BASE_URL}/${address}Create`, fields, {
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (result.data.message) {
+            dispatch(authFailed(result.data.message));
+        } else {
+            dispatch(stuffAdded(result.data));
+        }
+    } catch (error) {
+        dispatch(authError(error));
+    }
+};
